638eefcafd25db884442430b782dddf6d69b1891
balakrishna <kulalbalakrishna4@gmail.com>
2025-06-24 19:04:53 +0530

    removed critical files

--- app/src/main/java/com/RankRival/rankrival/test.java
+++ app/src/main/java/com/RankRival/rankrival/test.java
@@ -20,8 +20,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
